5476f4ce3bcd5dbafe9946c118496170b352a375
eric-sclafani <eric.sclafani321@gmail.com>
2023-07-09 18:36:51 -0400

    finished first seven exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    
+    if (num1 < 0 || num2 < 0 || typeof(num1) !== "number" || typeof(num2) !== "number" ){
+        return 'ERROR'
+    }
+    
+    let total = 0;
 
+    if (num1 < num2){
+        for (let i = num1; i <= num2; i++){
+            total += i;
+        }
+    } else {
+        for (let i = num2; i <= num1; i++){
+            total += i;
+        }
+    }
+    
+    return total
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let x = (degrees - 32) * (5/9);
+  return Math.round(x * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let x = (degrees * (9/5) + 32);
+  return Math.round(x * 10) / 10;
 };
 
 // Do not edit below this line
